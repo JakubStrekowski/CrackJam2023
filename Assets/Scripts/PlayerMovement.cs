5adce35facf84b8edfc864477ae455ccc8413c56
jakub.strekowski <jakub.strekowski@plum.pl>
2023-02-03 23:59:29 +0100

    Add dotween assembly

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speedModifier = 3f;
+
+    private Transform _transform;
+    private Vector3 _currentMovement;
+    private DefaultInputActions _playerInputs;
+
+    public void Movement ( InputAction.CallbackContext ctx )
+    {
+        _currentMovement = _playerInputs.Player.Move.ReadValue<Vector3>() * speedModifier;
+    }
+    
+    private void Awake()
+    {
+        _transform = transform;
+        _playerInputs = new DefaultInputActions();
+    }
+
+    private void FixedUpdate()
+    {
+        _transform.position = _transform.position + _currentMovement;
+    }
+}
