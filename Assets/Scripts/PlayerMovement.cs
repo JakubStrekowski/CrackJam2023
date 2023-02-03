bc7a3efbf57769529d9ba6a2658a6ee73f94a1ad
jakub.strekowski <jakub.strekowski@plum.pl>
2023-02-04 00:49:30 +0100

    Add basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,33 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using DG.Tweening;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
-//
-// public class PlayerMovement : MonoBehaviour
-// {
-//     [SerializeField] private float speedModifier = 3f;
-//
-//     private Transform _transform;
-//     private Vector3 _currentMovement;
-//     private DefaultInputActions _playerInputs;
-//
-//     public void Movement ( InputAction.CallbackContext ctx )
-//     {
-//         _currentMovement = _playerInputs.Player.Move.ReadValue<Vector3>() * speedModifier;
-//     }
-//     
-//     private void Awake()
-//     {
-//         _transform = transform;
-//         _playerInputs = new DefaultInputActions();
-//     }
-//
-//     private void FixedUpdate()
-//     {
-//         _transform.position = _transform.position + _currentMovement;
-//     }
-// }
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speedModifier = 0.4f;
+
+    private Transform _transform;
+    private Vector3 _currentMovement;
+
+    public void Movement ( InputAction.CallbackContext ctx )
+    {
+        Vector2 readValue = ctx.ReadValue<Vector2>() * speedModifier;
+        _currentMovement = new Vector3(readValue.x, 0, readValue.y);
+    }
+    
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentMovement != Vector3.zero)
+        {
+            _transform.position = _transform.position + _currentMovement;
+        }
+    }
+}
