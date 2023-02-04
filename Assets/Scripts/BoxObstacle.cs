7bc7ad26f02dcce6a5635154008a09e0b7e806dc
jakub.strekowski <jakub.strekowski@plum.pl>
2023-02-04 18:11:06 +0100

    Add obstacle box prefab

---
+++ Assets/Scripts/BoxObstacle.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BoxObstacle : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onSpawned;
+    [SerializeField] private float timeToRise = 1f;
+    [SerializeField] private float targetYPos = 0.5f;
+    [SerializeField] private float backYPos = -0.6f;
+    [SerializeField] private Ease riseEase;
+    [SerializeField] private Ease hideEase;
+
+    private Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+    
+    public void SetDisable()
+    {
+        _transform.DOMoveY(backYPos, timeToRise).SetEase(hideEase)
+            .onComplete = () => {gameObject.SetActive(false);};
+    }
+
+    private void OnEnable()
+    {
+        onSpawned?.Invoke();
+
+        _transform.DOMoveY(targetYPos, timeToRise).SetEase(riseEase);
+    }
+
+}
