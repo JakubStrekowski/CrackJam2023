7666d963dce789ce5169f7fe840a9f46db100f2f
jakub.strekowski <jakub.strekowski@plum.pl>
2023-02-04 13:00:13 +0100

    Add music settings, move main cam to prefabs

--- Assets/Scripts/MainMenuFunctions.cs
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuFunctions : MonoBehaviour
 {
+    [SerializeField] private AudioMixerGroup musicVolume;
+    [SerializeField] private AudioMixerGroup soundsVolume;
+
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider soundsSlider;
     public void StartPlaying()
     {
         SceneManager.LoadScene(1);
@@ -14,5 +22,22 @@ public class MainMenuFunctions : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Exit");
+    }
+
+    public void Start()
+    {
+        musicSlider.value = 0f;
+        OnMusicValueChange();
+        soundsSlider.value = 0f;
+        OnSoundsValueChange();
+    }
+
+    public void OnMusicValueChange()
+    {
+        musicVolume.audioMixer.SetFloat("MusicVol", musicSlider.value);
+    }
+    public void OnSoundsValueChange()
+    {
+        soundsVolume.audioMixer.SetFloat("SoundsVol", soundsSlider.value);
     }
 }
