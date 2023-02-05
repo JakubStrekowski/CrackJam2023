02018d79b88082c360feed44d51b23adf8ba1e61
jakub.strekowski <jakub.strekowski@plum.pl>
2023-02-05 03:12:37 +0100

    Move sprites to subdirectiories, Add sprites for root server, hide server after start hacking

--- Assets/Scripts/HackActivation.cs
+++ Assets/Scripts/HackActivation.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
@@ -8,8 +10,16 @@ public class HackActivation : MonoBehaviour
     [SerializeField] private GameObject hackText;
     [SerializeField] private PlayableDirector director;
     [SerializeField] private Slider slider;
+    [SerializeField] private GameObject particleSystemRoot;
 
+    private Collider col;
     private bool _wasActivated;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (_wasActivated) return;
@@ -27,9 +37,13 @@ public class HackActivation : MonoBehaviour
         if (_wasActivated) return;
         if (!hackText.activeInHierarchy) return;
 
+        particleSystemRoot.SetActive(false);
         hackText.SetActive(false);
         director.Play();
         _wasActivated = true;
         slider.gameObject.SetActive(true);
+
+        transform.DOMoveY(-1.5f, 2);
+        col.enabled = false;
     }
 }
